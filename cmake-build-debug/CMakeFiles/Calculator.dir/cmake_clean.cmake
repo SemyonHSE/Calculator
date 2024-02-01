ee7ae41c0b36510840cb0f602fba5e07b03b013e
averbusem <averbusem@gmail.com>
2024-02-01 21:06:39 +0300

    added an account

---
+++ cmake-build-debug/CMakeFiles/Calculator.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Calculator.dir/main.cpp.obj"
+  "CMakeFiles/Calculator.dir/main.cpp.obj.d"
+  "CMakeFiles/Calculator.dir/stack/stack.cpp.obj"
+  "CMakeFiles/Calculator.dir/stack/stack.cpp.obj.d"
+  "Calculator.exe"
+  "Calculator.exe.manifest"
+  "Calculator.pdb"
+  "libCalculator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include "cmath"
 
+using namespace std;
 
 int priority(char symbol);
 const double Pi = acos(-1);
@@ -10,10 +11,11 @@ double Sin(double x);
 double Cos(double x);
 double Tg(double x);
 double Ctg(double x);
-
+double calculation(Stack *NumberStack, Stack *OperatorStack);
 
 int main() {
-
+    /*NumberStack
+    OperatorStack*/
     return 0;
 }
 
@@ -45,3 +47,94 @@ double Ctg(double x) {
     double b = Sin(x);
     return (a / b);
 }
+double calculation(Stack *NumberStack, Stack *OperatorStack){
+    double operand2, operand1, rez;
+    operand2 = NumberStack->pop();
+    if (OperatorStack->show_top()=='+'){
+        operand1 = NumberStack->pop();
+        rez = operand1 + operand2;
+        NumberStack->push(rez);
+        OperatorStack->pop();
+    }
+    else if(OperatorStack->show_top()=='-'){
+        operand1 = NumberStack->pop();
+        rez = operand1 - operand2;
+        NumberStack->push(rez);
+        OperatorStack->pop();
+    }
+    else if(OperatorStack->show_top()=='*'){
+        operand1 = NumberStack->pop();
+        rez = operand1 * operand2;
+        NumberStack->push(rez);
+        OperatorStack->pop();
+    }
+    else if(OperatorStack->show_top()=='/'){
+        if (operand2 ==0){
+            cerr << "\nIt cannot be divided by 0!\n";
+            return -1;
+        }
+        operand1 = NumberStack->pop();
+        rez = operand1 / operand2;
+        NumberStack->push(rez);
+        OperatorStack->pop();
+    }
+    else if(OperatorStack->show_top()=='^'){
+        operand1 = NumberStack->pop();
+        rez = pow(operand1, operand2);
+        NumberStack->push(rez);
+        OperatorStack->pop();
+    }
+    else if (OperatorStack->show_top()=='s'){
+        rez = sin(operand2);
+        NumberStack->push(rez);
+        OperatorStack->pop();
+    }
+    else if (OperatorStack->show_top()=='c'){
+        rez = cos(operand2);
+        NumberStack->push(rez);
+        OperatorStack->pop();
+    }
+    else if(OperatorStack->show_top()=='t'){
+        if (cos(operand2)==0){
+            cerr << "\nInvalid argument for tangent!\n";
+            return -1;
+        }
+        rez = tan(operand2);
+        NumberStack->push(rez);
+        OperatorStack->pop();
+    }
+    else if(OperatorStack->show_top()=='g'){
+        if (sin(operand2)==0){
+            cerr << "\nInvalid argument for cotangence!\n";
+            return -1;
+        }
+        rez = Ctg(operand2);
+        NumberStack->push(rez);
+        OperatorStack->pop();
+    }
+    else if(OperatorStack->show_top()=='e'){
+        rez = exp(operand2);
+        NumberStack->push(rez);
+        OperatorStack->pop();
+    }
+    else{
+        cerr<<"Error";
+        return -1;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
