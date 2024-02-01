9d2b3a4015f331ea7167880cc235c088b66442ad
Kirill <kmonyov@mail.ru>
2024-02-01 20:00:44 +0300

    Trigonometric functions has created

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include "stack/stack.h"
 #include <string>
+#include "cmath"
 
+
+int priority(char symbol);
+const double Pi = acos(-1);
+double Sin(double x);
+double Cos(double x);
+double Tg(double x);
+double Ctg(double x);
+
+
+int main() {
+
+    return 0;
+}
 
 int priority(char symbol) {
     if (symbol == 's' || symbol == 'c' || symbol == 't' || symbol == 'g' || symbol == 'e')
@@ -15,8 +29,19 @@ int priority(char symbol) {
     else return 0;
 }
 
-
-int main() {
-
-    return 0;
+double Sin(double x) {
+    return (round(sin(x) * 10000000) / 10000000);
+}
+double Cos(double x) {
+    return (round(cos(x) * 10000000) / 10000000);
+}
+double Tg(double x) {
+    double a = Sin(x);
+    double b = Cos(x);
+    return (a / b);
+}
+double Ctg(double x) {
+    double a = Cos(x);
+    double b = Sin(x);
+    return (a / b);
 }
