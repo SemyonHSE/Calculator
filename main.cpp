a69458688f157032fc696a2786fd98181332a616
Kirill <kmonyov@mail.ru>
2024-02-01 19:56:07 +0300

    Added function to definiton priority

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 #include "stack/stack.h"
 #include <string>
 
+
+int priority(char symbol) {
+    if (symbol == 's' || symbol == 'c' || symbol == 't' || symbol == 'g' || symbol == 'e')
+        return 4;
+    else if (symbol == '^')
+        return 3;
+    else if (symbol == '+' || symbol == '-')
+        return 1;
+    else if (symbol == '*' || symbol == '/')
+        return 2;
+    else return 0;
+}
+
+
 int main() {
 
     return 0;
