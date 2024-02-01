02c61e2cbf52986cf289ae4cbf02e0449590ea4f
Masterfar7 <145840792+Masterfar7@users.noreply.github.com>
2024-02-01 21:59:51 +0300

    Функции(недоделано)

--- main.cpp
+++ main.cpp
@@ -13,7 +13,64 @@ double Cos(double x);
 double Tg(double x);
 double Ctg(double x);
 bool calculation(Stack *NumberStack, Stack *OperatorStack);
-
+std::string replaceSymbolWithNumber(std::string& a) {
+    int ln = a.length();
+    //std::string zam = std::to_string(b);
+    int flag = 1;
+    while (flag)
+    {
+        flag = 0;
+        if (97<=a[0] && a[0]<=122 && (a[1] < 97 || a[1]>122))
+        {
+            std::string param;
+            std::cout << a[0] << '=';
+            std::cin >> param;
+            char tek = a[0];
+            for (int j = 1; j < ln; ++j)
+            {
+                if (a[j] == tek && (a[j - 1] < 97 || a[j - 1]>122) && (a[j + 1] < 97 || a[j + 1]>122))
+                {
+                    a.replace(j, 1, param);
+                }
+            }
+            a.replace(0, 1, param);
+        }
+        for (int i = 1; i < ln; ++i)
+        {
+            if (a[i] >= 97 && a[i] <= 122 && (a[i - 1] < 97 || a[i - 1]>122) && (a[i + 1] < 97 || a[i + 1]>122))
+            {
+                std::string param;
+                std::cout << a[i] << '=';
+                std::cin >> param;
+                char tek = a[i];
+                for (int j = i + 1; j < ln; ++j)
+                {
+                    if (a[j] == tek && (a[j - 1] < 97 || a[j - 1]>122) && (a[j + 1] < 97 || a[j + 1]>122))
+                    {
+                        a.replace(j, 1, param);
+                    }
+                }
+                a.replace(i, 1, param);
+            }
+        }
+        ln = a.length();
+        if (97 <= a[0] && a[0] <= 122 && (a[1] < 97 || a[1]>122))
+        {
+            flag = 1;
+        }
+        else
+        {
+            for (int i = 1; i < ln; ++i)
+            {
+                if (a[i] >= 97 && a[i] <= 122 && (a[i - 1] < 97 || a[i - 1]>122) && (a[i + 1] < 97 || a[i + 1]>122))
+                {
+                    flag = 1;
+                }
+            }
+        }
+    }
+    return a;
+}
 int main() {
 
     Stack NumberStack;
